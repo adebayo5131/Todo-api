f2d6bd00b948cdc3b48bcfa93ee89b25efe722cd
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-06-28 21:43:08 -0400

    new file

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+
+//     name : 'Adebayo',
+//     age: 21
+// };
+
+// function updatePerson (obj){
+
+//     obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 27];
+
+
+function addGrades (grades)
+{
+    grades.push(55,44,8);
+    debugger;
+}
+
+addGrades(grades);
+console.log(grades)
