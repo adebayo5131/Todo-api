cf6af011ce786115ce77745131c9021e05570bde
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-03 00:04:15 -0400

    updated files

--- db.js
+++ db.js
@@ -1,41 +1,22 @@
 var Sequelize = require('sequelize');
-
-
-var env =process.env.NODE_ENV || 'development';
-
+var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-//Heroku environment
-if(env ==='production'){
-
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-
-        dialect: 'postgres'
-    })
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
 }
 
-//not running on Heroku
-else{
+var db = {};
 
-    //instance of sequelize
- sequelize = new Sequelize(undefined,undefined,undefined,{
-     
-  'dialect': 'sqlite',
-  'storage':__dirname +'/data/dev-todo-api.sqlite'
-});
-
-
-}
-
-
-var db ={};
-
-//loading sequelize from seperate files
 db.todo = sequelize.import(__dirname + '/models/todo.js');
-
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-//exporting db object
-module.exports =db;
+module.exports = db;
